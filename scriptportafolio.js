3e053e6abe95ee6a63cd3977f79093b59bcf153c
SergioMMQ <quetzal.mq97@gmail.com>
2024-11-06 00:37:21 -0600

    foter unificado

---
+++ scriptportafolio.js
@@ -0,0 +1,116 @@
+// Cargar imágenes en la galería
+const images = [
+    "Diseño 0.jpg",
+    "Diseño 1.png",
+    "Diseño 2.jpg",
+    "Diseño 3.jpg",
+    "Diseño 4.jpg",
+    "Diseño 5.jpg",
+    "Diseño 6.jpg",
+    "Diseño 7.jpg",
+    "Diseño 8.jpg",
+    "Diseño 9.jpg",
+    "Diseño 10.jpg",
+    "Diseño 11.jpg",
+    "Diseño 12.jpg",
+    "Diseño 13.jpg",
+    "Diseño 14.jpg",
+    "Diseño 15.jpg",
+    "Diseño 16.jpg",
+    "Diseño 17.jpg",
+    "Diseño 18.jpg",
+    "Diseño 19.jpg",
+    "Diseño 20.jpg",
+    "Diseño 21.jpg",
+    "Diseño 22.jpg",
+    "Diseño 23.jpg",
+    "Diseño 24.jpg",
+    "Diseño 25.jpg",
+    "Diseño 26.jpg",
+    "Diseño 27.jpg",
+    "Diseño 28.jpg",
+    "Diseño 29.jpg",
+    "Diseño 30.jpg",
+    "Diseño 31.jpg",
+    "Diseño 32.jpg",
+    "Diseño 33.jpg",
+    "Diseño 34.jpg",
+    "Diseño 35.jpg",
+    "Diseño 36.jpg",
+    "Diseño 37.jpg",
+    "Diseño 38.jpg",
+    "Diseño 39.jpg",
+    "Diseño 40.jpg",
+    "Diseño 41.jpg",
+    "Diseño 42.jpg",
+    "Diseño 43.jpg",
+    "Diseño 44.jpg",
+    "Diseño 45.jpg",
+    "Diseño 46.jpg",
+    "Diseño 47.jpg",
+    "Diseño 48.jpg",
+    "Diseño 49.jpg",
+    "Diseño 50.jpg",
+    "Diseño 51.jpg",
+    "Diseño 52.jpg",
+    "Diseño 53.jpg",
+    "Diseño 54.jpg",
+    "Diseño 55.jpg",
+    "Diseño 56.jpg",
+    "Diseño 57.jpg",
+    "Diseño 58.jpg",
+    "Diseño 59.jpg",
+    "Diseño 60.jpg",
+    "Diseño 61.jpg",
+    "Diseño 62.jpg",
+];
+
+const galeria = document.getElementById('galeria');
+
+images.forEach(image => {
+    const item = document.createElement('div');
+    item.classList.add('item');
+    
+    const img = document.createElement('img');
+    img.src = `img/${image}`;
+    img.alt = image.split('.')[0].replace('-', ' ');
+    
+    // Evento para abrir el lightbox al hacer clic en la imagen
+    img.onclick = function() {
+        openLightbox(this.src, this.alt);
+    };
+    
+    const p = document.createElement('p');
+    p.textContent = img.alt;
+
+    item.appendChild(img);
+    item.appendChild(p);
+    galeria.appendChild(item);
+});
+
+// Función para abrir el lightbox
+function openLightbox(src, alt) {
+    const lightbox = document.getElementById('lightbox');
+    const lightboxImg = document.getElementById('lightbox-img');
+
+    lightbox.style.display = "block";
+    lightboxImg.src = src;
+    lightboxImg.alt = alt;
+}
+
+// Cerrar el lightbox
+document.querySelector('.close').onclick = function() {
+    document.getElementById('lightbox').style.display = "none";
+};
+
+// Cerrar el lightbox al hacer clic fuera de la imagen
+document.getElementById('lightbox').onclick = function(event) {
+    if (event.target === this) {
+        this.style.display = "none";
+    }
+};
+
+function closeAd() {
+    document.getElementById("sidebarAd").style.display = "none";
+}
+

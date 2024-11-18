350c23332775d82e221ca0e7eb493c220a196634
SergioMMQ <quetzal.mq97@gmail.com>
2024-11-17 18:09:23 -0600

    .

--- eventos.js
+++ eventos.js
@@ -27,7 +27,7 @@ images.forEach(image => {
     item.classList.add('item');
     
     // Asigna la imagen de fondo al div
-    item.style.minWidth = '300px'; // Ancho mínimo de 200px
+    item.style.minWidth = '260px'; // Ancho mínimo de 200px
     item.style.maxWidth = '350px'; // Ancho maximo de 350px
     item.style.height = '350px'; // Altura del div
     item.style.backgroundImage = `url('imgeventos/${image}')`;
